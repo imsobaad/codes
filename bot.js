9c17ee40616586b1b4df49f019df066591ba568b
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-11 15:51:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,60 +56,6 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-if (message.content.startsWith(prefix + 'help')) {
-    let pages = ['╔╗╔═╦══╦═╗─╔╦═══╗╔══╗╔═══╦════╗
-║║║╔╩╣╠╣║╚╗║║╔═╗║║╔╗║║╔═╗║╔╗╔╗║
-║╚╝╝─║║║╔╗╚╝║║─╚╝║╚╝╚╣║─║╠╝║║╚╝
-║╔╗║─║║║║╚╗║║║╔═╗║╔═╗║║─║║─║║
-║║║╚╦╣╠╣║─║║║╚╩═║║╚═╝║╚═╝║─║║
-╚╝╚═╩══╩╝─╚═╩═══╝╚═══╩═══╝─╚╝',
-		 'page2','page3']
-    
-    
-    
-    let page = 1;
-
-    let embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setFooter(`Page ${page} of ${pages.length}`)
-    .setDescription(pages[page-1])
-
-    message.channel.sendEmbed(embed).then(msg => {
-
-        msg.react('◀').then( r => {
-            msg.react('▶')
-
-
-        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
-        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
-
-
-        const backwards = msg.createReactionCollector(backwardsFilter, { time: 20000});
-        const forwards = msg.createReactionCollector(forwardsFilter, { time: 20000});
-
-
-
-        backwards.on('collect', r => {
-            if (page === 1) return;
-            page--;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        forwards.on('collect', r => {
-            if (page === pages.length) return;
-            page++;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        })
-    })
-    }
-});
-
-
 
 
 
