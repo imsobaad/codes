21b61b8f2344ce30d4d1fd2642f8d430ec4fc9a1
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-12 12:26:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,68 +57,6 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 });
 
 
-client.on('message', async message => {
-if(message.author.bot) return;
-if (message.channel.guild) {
-if (message.content === '*level') {
- const embed = new Discord.RichEmbed()  
-       .setColor("RANDOM") 
-      .setDescription(`
-Your XP : ${voice[message.member.id].xp}
-Your Level : ${voice[message.member.id].level}
-	  `)
-   message.channel.sendEmbed(embed)
-      fs.writeFile('./voiceState.json', JSON.stringify(voice, null, 4), (e) => {
-        if(e) console.log(e);
-      });
-}}});
-
-
-
-
-  var returned;
-client.on('voiceStateUpdate', (user, member) => {
-  if(member.selfDeaf || member.selfMute || member.serverDeaf || member.serverMute) {
-    returned = false;
-  }
-  if(!member.selfDeaf || !member.selfMute ||!member.serverDeaf || !member.serverMute) {
-    returned = true;
-  }
-  setInterval(() => {
-    if(returned === true) {
-      if(member.bot) return;
-      if(!member.voiceChannel) returned = false;
-      if(!voice[member.id]) voice[member.id] = {
-        xp: 1,
-        level: 1
-      };
-      voice[member.id] = {
-        xp: voice[member.id].xp + Math.floor(Math.random() * 4) + 1,
-        level: voice[member.id].level
-      };
-      var curXp = voice[member.id].xp;
-      var curLvl = voice[member.id].level;
-      if(curXp >= 300) {
-        voice[member.id] = {
-          xp: 1,
-          level: curLvl + 1
-        };
-      }
-      fs.writeFile('./voiceState.json', JSON.stringify(voice, null, 4), (e) => {
-        if(e) console.log(e);
-      });
-    } else if(returned === false) {
-      return null;
-    }
-  },5000);
-});
-
-
-
-
-
-
-
    client.on('message', message => {
     const prefix = '*'
 var args = message.content.split(" ").slice(1);    
