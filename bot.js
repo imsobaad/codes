1f1f7ca2230f1323fcfc52f1aaa9adc1d11718ef
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-14 17:04:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -170,6 +170,30 @@ if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return mess
 
 
 
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const yumz = member.guild.channels.find("name", "welcome");
+     yumz.send(`<@${inviter.id}> تم دعوته بواسطة <@${member.user.id}>`);
+   //  yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
+
+
+
 
 client.on('message', function(msg) {
     if(msg.content.startsWith (prefix  + 'server')) {
