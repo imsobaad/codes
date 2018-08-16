f63b567296c5a22f023503e90c2129d0866c3b0b
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-16 20:49:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,44 +59,6 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 
 
 
-
-let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
-let banse = new Set();
-client.on('guildBanAdd', function(guild) {
-  guild.fetchAuditLogs().then(logs => {
-    const ser = logs.entries.first().executor;
-    if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
-      bans: 5
-    }
-    let boner = bane[ser.id+guild.id]
-banse.add(ser.id)
-boner.bans = Math.floor(boner.bans+1)
-
-
-setTimeout(() => {
-  boner.bans = 5
-  banse.delete(ser.id)
-},8000)
-
-if(boner.bans > 5) {
-  let roles = guild.members.get(ser.id).roles.array()
-guild.members.get(ser.id).removeRoles(roles)
-}
-
-    })
-    fs.writeFile('./bcer.json', JSON.stringify(bane), (err) => {
-if (err) console.error(err);
-})
-
-})
-
-
-
-
-
-
-
-
 client.on('message',async message => {
   var time = moment().format('Do MMMM YYYY , hh:mm');
   var room;
