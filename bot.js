98ec94528ebc610cd924f5591d258c0617bfb1b6
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-25 18:32:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -286,7 +286,7 @@ client.on("guildMemberAdd", member => {
         .setAuthor(h.username,h.avatarURL)
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
 	.setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:bad});          
+     welcomer.send({embed});          
                
  
       }
