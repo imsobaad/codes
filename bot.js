a51578de0930d6673e23406f8cecc4bac697cc46
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-27 17:53:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -299,7 +299,7 @@ let welcomer = member.guild.channels.find("name","welcome");
         let norelden = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setThumbnail(h.avatarURL)
-        .addField('Created Date :',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+        .addField('Created Date :',`${moment(member.user.createdAt).format('D/M/YYYY h:mm ')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
          .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:norelden});          
                
