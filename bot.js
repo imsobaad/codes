762e84397a24c79f9863106057cc0bb96d0ba821
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-09-04 18:18:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 
 
 client.on('message',async msg => {
-  var p = "!";
+  var p = "*";
   if(msg.content.startsWith(p + "user")) {
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
@@ -81,6 +81,29 @@ Dat = currentTime.getDate()
  
 });
 
+
+
+client.on('message',async msg => {
+  var p = "*";
+  if(msg.content.startsWith(p + "bot")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.bots.size} ← ◢`);
+ },1000);
+  });
+  }
+ 
+});
 
 
 
