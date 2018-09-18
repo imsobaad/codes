9571b5defff23b6e4125e5925ff5230a3a57648c
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-09-18 21:37:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -203,6 +203,28 @@ message.channel.send({embed});
 
 
 
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith("*avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
 client.on('message', async message =>{
 const ms = require("ms");
 if (message.author.omar) return;
