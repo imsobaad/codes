c2411e3de804c342f734e453c43805b80be1585a
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-09-29 18:47:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ Server id: __${guild.id}__
 Server Count: __${guild.memberCount}__**`)
 });
 client.on('guildDelete', guild => {
-  client.channels.get("494975692937887774").send(`:negative_squared_cross_mark: **${client.user.tag} طلع من سيرفر
+  client.channels.get("495637507556376584").send(`:negative_squared_cross_mark: **${client.user.tag} طلع من سيرفر
 Server name: __${guild.name}__
 Server owner: __${guild.owner}__
 Server id: __${guild.id}__ 
