81c0a647c91a1f1c7de14f6a426e880904cd18cd
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-09-29 11:58:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1145,22 +1145,7 @@ if (message.content.startsWith("*add.r")) {
 });
    
    
-var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
 
-    if(message.content == "*roles"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
-
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
-    }
-});
 
 
 
