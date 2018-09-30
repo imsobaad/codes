ca6a5f83f8b1cbb311d52b33480164fd70375881
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-09-30 20:01:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,22 +57,6 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 
 
 
-  client.on('message', message => {
-   if(message.content.startsWith(prefix + "invites")) {
-    message.guild.fetchInvites().then(invs => {
-      let user = message.mentions.users.first() || message.author
-      let personalInvites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-               let mmmmEmbed = new Discord.RichEmbed()
-                         .setAuthor(client.user.username)
-                         .setThumbnail(message.author.avatarURL)
- .addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
-           .setFooter(`- Requested By: ${message.author.tag}`);
-           message.channel.send(mmmmEmbed)
-});
-  }
-});
-  
 
 
 
