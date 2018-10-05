ee174ca08f69b627049678c385d9601d805d326b
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-10-05 17:18:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -658,46 +658,6 @@ let PREFIX = '*'
 
 
 
-function timeCon(time) {
-    let days = Math.floor(time % 31536000 / 86400)
-    let hours = Math.floor(time % 31536000 % 86400 / 3600)
-    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
-    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
-    days = days > 9 ? days : '0' + days
-    hours = hours > 9 ? hours : '0' + hours
-    minutes = minutes > 9 ? minutes : '0' + minutes
-    seconds = seconds > 9 ? seconds : '0' + seconds
-    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
-}
-  client.on('message',async message => {
-  if(message.content.startsWith(prefix + "bot")) {   
-	 message.channel.send({
-	   embed: new Discord.RichEmbed()
-	   .setColor("RANDOM")
-      .setAuthor(message.author.username, message.author.avatarURL) 
-      .addField('= Normal Information =')
-      .addField('Creator :: ${message.users.get("486200045008453635").username} - ${moment(message.user.createdAt).fromNow()}')
-      .addField('Ping :: ${message.pings[0]} ms')
-      .addField('UpTime :: [timeCon(process.uptime())]')
-      .addField('= Servers Information =')
-      .addField('Servers :: ${message.guilds.size}')
-      .addField('Users :: ${message.users.size}')
-      .addField('Channels :: ${message.channels.size}')
-      .addField('= Developer Information =')
-      .addField('NodeJS :: ${process.version}')
-      .addField('DiscordJS :: ${Discord.version}')
-      .addField('Arch :: ${process.arch}')
-      .addField('Platform :: ${process.platform}')
-      .addField('= Host Information =')
-      .addField('UsedHeap :: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB')
-      .addField('Heap :: ${Math.round(process.memoryUsage().heapTotal / 1024 / 1024 * 100) / 100} MB')
-      .addField('Ram :: [${(process.memoryUsage().rss / 1048576).toFixed()} MB')
-      .addField('Rss :: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100} MB')
-
-    })
-}
-});
-
 
    
    
