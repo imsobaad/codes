f083b60f64f1e3c53489c59b3daa55846dd7f9ff
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-10-05 17:36:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,6 +60,42 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 
 
 
+
+function timeCon(time) {
+    let days = Math.floor(time % 31536000 / 86400)
+    let hours = Math.floor(time % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+    days = days > 9 ? days : '0' + days
+    hours = hours > 9 ? hours : '0' + hours
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds
+    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+}
+client.on('message', message => {
+    if (message.content.startsWith("*bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setColor('RANDOM')
+	    .addField('``Name``' , `[ ${client.user.tag} ]` , true)
+	    .addField('``ID``' , `[ ${client.user.id} ]` , true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+	    .addField('``Uptime``', [timeCon(process.uptime())], true)
+
+    })
+}
+});
+
+
+
+
+
+
 const invites = {};
 const wait = require('util').promisify(setTimeout);
 client.on('ready', () => {
