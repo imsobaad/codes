16e112ffbe513ee8313226c4f3c77da63b446add
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-10-12 20:41:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1127,8 +1127,7 @@ client.on('message', msg => {
     let successCount = 0;
     message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
       message.guild.members.forEach(g => {
-	  g.send.forEach(m => {
-		      var bc = new Discord.RichEmbed()
+      var bc = new Discord.RichEmbed()
        .setColor('RANDOM')
        .setTitle('__**📢Broadcast📢**__') 
        .addField('**🔸Server**', message.guild.name)
@@ -1136,8 +1135,7 @@ client.on('message', msg => {
        .addField('**📖Message**', args)
        .setTimestamp() 
        .setFooter(copy, client.user.avatarURL);
-    m.sendMessage({ embed: bc })
-	  })
+    g.sendMessage({ embed: bc })
           successCount++;
           msgCount++;
           msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
