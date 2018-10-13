066bf1ce0e1288de64142bde22f3a06eb0e60b1c
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-10-13 18:30:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,6 +58,21 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 
 
 
+client.on('message', message => {
+  if(message.author.bot) return;
+    if(message.content === prefix + 'info') {
+      var sg = client.guilds.filter(o => o.memberCount > 100).map(e => e.name).join('\n')
+      var gl = client.guilds.filter(g => g.memberCount < 100).map(n => n.name).join('\n')
+      var gm = new Discord.RichEmbed()
+      .setDescription('- قائمة اعضاء السيرفرات')
+      .setColor('RANDOM')
+      .addField('- قائمة السيرفرات التي تملك فوق المئة عضو', sg || "0")
+      .addField('- قائمة السيرفرات التي تملك اقل من مئة عضو', gl || "0")
+      .setFooter(Guilds: ${client.guilds.size}, Users: ${client.users.size}, Channels: ${client.channels.size}.)
+      message.channel.send(gm);
+    }
+});
+
 
 
 
