9d89d0dcc22c8dceb7ccaa4ccef6af382fb2b838
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-02 18:42:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,21 +27,6 @@ const sql = require("sqlite");
 ,ti={}  
 ,spee={};
  const prefix = "*";
-const client = new Discord.Client({disableEveryone: true, maxMessagesCache: 1});
-const config = { prefix: "*" };
-const tpoints = {};
-const vpoints = {};
-client.config = config;
-client.login(client.config.token);
-client.on('ready',async () => {
-  console.log(`HI`);
-  client.users.forEach(m => {
-    if(m.bot) return;
-    if(!tpoints[m.id]) tpoints[m.id] = {points: 0, id: m.id};
- 
-    if(!vpoints[m.id]) vpoints[m.id] = {points: 0, id: m.id};
-  });
-});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -92,52 +77,6 @@ client.on('message', message => {
 }
 });
 
-
-
-
- 
-client.on('message',async message => {
-  if(message.author.bot || message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  let member = message.member;
-  let mention = message.mentions.users.first();
-  let guild = message.guild;
-  let author = message.author;
- 
-  let rPoints = Math.floor(Math.random() * 4) + 1;// Random Points
-  tpoints[author.id].points += rPoints;
-  if(args[0] === `${client.config.prefix}top`) {
-    let _voicePointer = 1;
-    let _textPointer = 1;
-    let _voiceArray = Object.values(vpoints);
-    let _textArray = Object.values(tpoints);
-    let _topText = _textArray.slice(0, 5).map(r => `**\`.${_textPointer++}\` | <@${r.id}> \`XP: ${r.points}\`**`).sort((a, b) => a > b).join('\n');
-    let _voiceText = _voiceArray.slice(0, 5).map(r => `**\`.${_voicePointer++}\` | <@${r.id}> \`XP: ${r.points}\`**`).sort((a, b) => a > b).join('\n');
- 
-    let topRoyale = new Discord.RichEmbed();
-    topRoyale.setAuthor(message.author.username, message.author.avatarURL);
-    topRoyale.setTitle('# " Top');
-    //topRoyale.setThumbnail(message.guild.iconURL);
-    topRoyale.addField(`**TOP 5 TEXT 💬**`, _topText, true);
-    topRoyale.addField(`**TOP 5 VOICE 🎙**`, _voiceText, true);
-    topRoyale.setFooter(`test`, message.guild.iconURL);
-    message.channel.send(topRoyale).catch(e => {
-      if(e) return message.channel.send(`**. Error; \`${e.message}\`**`);
-    });
-  }
-});
- 
-client.on('voiceStateUpdate', (u, member) => {
-  let author = member.user.id;
-  let guild = member.guild;
-  if(member.voiceChannel === null) return;
-  let rPoints = Math.floor(Math.random() * 4) + 1;// Random Points
-  setInterval(() => {
-    if(!member.voiceChannel) return;
-    if(member.selfDeafen) return;
-    vpoints[author].points += rPoints;
-  }, 5000); // 5 Secs
-});
 
 
 
