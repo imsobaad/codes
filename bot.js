2f1015eb11fa4eec7f28818524b33400ee255e2a
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-15 22:21:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,10 +62,9 @@ client.on('message', message => {
     if (message.content.startsWith('#bc')){
   if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-  message.channel.sendMessage('عضو ${message.guild.memberCount} يتم ارسال البرودكاست الى  ')
+  message.channel.sendMessage('يتم ارسال البرودكاست الى اعضاء السيرفر ')
   message.guild.members.forEach(m => {
-  m.send( "${guild.member}" + "\n" +
- "**" +`${args}` + "**")
+  m.send(args)
 
 })
 }
