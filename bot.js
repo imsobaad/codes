02103002e8d95cb29fee012deda03fa3c3f57e4a
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-16 14:19:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,19 +56,6 @@ client.user.setGame(`Use *help`,"http://twitch.tv/S-F")
 
 
 
-client.on('message', message => {
-    if(!message.channel.guild) return;
-    let args = message.content.split(' ')
-    if (message.content.startsWith('#bc')){
-  if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-  message.channel.sendMessage('يتم ارسال البرودكاست الى اعضاء السيرفر ')
-  message.guild.members.forEach(m => {
-  m.send(args)
-
-})
-}
-});
 
 
 
